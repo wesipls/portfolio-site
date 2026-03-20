0c3379760d805ff6f177edb93b42c68d0c6effc6
Kim Valkama <hello@valkama.kim>
2026-03-20 15:57:24 +0200

    Added some easter eggs, cause why not?

--- script.js
+++ script.js
@@ -8,3 +8,7 @@ function prepareAndPrint() {
     // Restore the original browser tab title
     document.title = originalTitle;
 }
+console.log(
+    `%cHope you like Vim; if we work together, that's going to be your new default server editor.`, 
+    "color: #4af626; font-family: monospace; font-size: 13px; line-height: 1.5;"
+);
